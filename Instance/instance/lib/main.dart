030358450ce22a78b5e914e1b74fe026856e9909
Pak_Man926 <davidkamau926@gmail.com>
2025-02-26 11:16:46 +0300

    Changes 2

--- Instance/instance/lib/main.dart
+++ Instance/instance/lib/main.dart
@@ -5,12 +5,76 @@ void main()
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget
+class MyApp extends StatefulWidget
 {
-  const MyApp({super.key});
+  @override
+  _MyAppState createState()
+  {
+    return _MyAppState();
+  }
+}
 
+class _MyAppState extends State<MyApp>
+{
+  var _questionIndex = 0;
+
+  var _questions =[
+    "What color do you like the most?",
+    "What\'s your favourite weather?"
+  ];
+
+  var _answers =[
+    ["Red","Green","Blue"],
+    ["Sunny","Rainy","Calm"]
+  ];
+
+  void _answerQuestions(String answer)
+  {
+    setState(()
+    {
+      if (_questionIndex < 2)
+      {
+        _questionIndex += _questionIndex; 
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context)
   {
-    return MaterialApp(home: Text("Hello Champ"));
+    if (_questionIndex < 2)
+    {
+      return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(title: Text("Personality Test")),
+          body: Column(
+            children: [
+              Text(_questions[_questionIndex]),
+              ElevatedButton(
+                onPressed: () => _answerQuestions(_answers[_questionIndex][0]),
+                 child: Text(_answers[_questionIndex][0])
+                 ),
+              ElevatedButton(
+                onPressed: () => _answerQuestions(_answers[_questionIndex][1]),
+                 child: Text(_answers[_questionIndex][1])
+                 ),
+            ],
+          ),
+        ),
+      );
+    }
+    else
+    {
+      return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(title: Text("Liar Test")),
+          body: Column(
+            children: [
+              Text("We don't know anything about you, but the app looks great!!"),
+            ],
+          ),
+        )
+      );
+    }
   }
 }
