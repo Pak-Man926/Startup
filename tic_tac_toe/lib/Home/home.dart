5b3b61fe9f8a7fcdc6267161fae9bdd1b8d8cadc
Pak_Man926 <davidkamau926@gmail.com>
2025-03-19 14:25:42 +0300

    Third 3.0

--- tic_tac_toe/lib/Home/home.dart
+++ tic_tac_toe/lib/Home/home.dart
@@ -55,6 +55,80 @@ class _HomeState extends State<Home>
 
   bool _checkWinner(String players)
   {
-    for  
+    for (int i =0; i<3; i++)
+    {
+      if(board[i * 3] == players && board[1 * 3 + 1] == players && board[i * 3 + 2] == players)
+      {
+        return true;
+      }
+      if(board[i] == players && board[i + 3] == players && board[i + 6] == players)
+      {
+        return true;
+      }
+      if(board[0] == players && board[4] == players && board[8] == players)
+      {
+        return true;
+      }
+      if(board[2] == players && board[4] == players && board[6] == players)
+      {
+        return true;
+      }
+    }
   }
+
+  bool _checkDraw()
+  {
+    return board.every((cell) => cell != "");
+  }
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tic Tac Toe"): 
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            winner != "" ? Text("Winner: $winner", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)) 
+            : isDraw ? 
+            const Text("Draw", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold))
+            : Text("Current Player: $currentPlayer", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+                itemCount: 9,
+                itemBuilder: (BuildContext context, int index)
+                {
+                  return GridTile(
+                    child: Container(
+                      margin: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black)
+                      ),
+                      child: MaterialButton(
+                        onPressed: () => _handleTap(index),
+                        child: Text("board[index]", style: TextStyle(fontSize: 40))
+                        )
+                    )
+                  );
+                }
+
+              )
+                
+            ),
+            MaterialButton(
+              child: const Text("Restart Game"),
+              onPressed: () {
+                setState(() {
+                  _initializeGame();
+                });
+              }
+            )
+        ],
+        )
+    );
+  }
+
 }
