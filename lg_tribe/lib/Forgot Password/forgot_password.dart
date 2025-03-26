b043115ed4f9b556167656133e2c9b6f96889ce2
Pak_Man926 <davidkamau926@gmail.com>
2025-03-26 16:36:42 +0300

    Eighth 7.4

--- lg_tribe/lib/Forgot Password/forgot_password.dart
+++ lg_tribe/lib/Forgot Password/forgot_password.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:phonenumber_input/phonenumber_input.dart";
 
 class Password extends StatelessWidget {
   const Password({super.key});
@@ -12,22 +13,56 @@ class Password extends StatelessWidget {
           child: Column(
             //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
+              SizedBox(height: 120),
               Container(
                 //height: 50,
                 //width: 50,
                 alignment: Alignment.center,
                 padding: EdgeInsets.only(left: 50, right: 50),
                 decoration: BoxDecoration(
-                  color: Colors.green,
+                  color: const Color.fromARGB(255, 175, 76, 92),
                   //borderRadius: BorderRadius.circular(50),
                   shape: BoxShape.circle,
                 ),
                 child: Icon(Icons.key, size: 50, color: Colors.white),
               ),
-              SizedBox(height: 20),
+              SizedBox(height: 40),
               Text(
                 "Forgot Password?",
                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 10),
+              Text(
+                "No worries, we\'ll send in your reset instructions",
+                style: TextStyle(fontSize: 12, fontWeight: FontWeight.w100),
+              ),
+              SizedBox(height: 20),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Text(
+                      "Registered Phone Number",
+                      style: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: PhoneNumberInput(
+                      labelText: "Phone Number",
+                      onCountryCodeChanged: (code) {
+                        print('Country code changed: $code');
+                      },
+                      onPhoneNumberChanged: (phoneNumber) {
+                        print('Phone number changed: $phoneNumber');
+                      },
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
