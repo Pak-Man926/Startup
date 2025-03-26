926ab5f392a422f776dc2a9deb1caaf769cd777b
Pak_Man926 <davidkamau926@gmail.com>
2025-03-26 16:14:26 +0300

    Eighth 7.3

--- lg_tribe/lib/Forgot Password/forgot_password.dart
+++ lg_tribe/lib/Forgot Password/forgot_password.dart
@@ -1,13 +1,10 @@
 import "package:flutter/material.dart";
 
-
-class Password extends StatelessWidget
-{
+class Password extends StatelessWidget {
   const Password({super.key});
 
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
@@ -15,29 +12,27 @@ class Password extends StatelessWidget
           child: Column(
             //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-               Container(
-                height: 50,
+              Container(
+                //height: 50,
                 //width: 50,
                 alignment: Alignment.center,
                 padding: EdgeInsets.only(left: 50, right: 50),
                 decoration: BoxDecoration(
                   color: Colors.green,
-                  borderRadius: BorderRadius.circular(50),
+                  //borderRadius: BorderRadius.circular(50),
+                  shape: BoxShape.circle,
                 ),
                 child: Icon(Icons.key, size: 50, color: Colors.white),
-               ),
+              ),
               SizedBox(height: 20),
               Text(
                 "Forgot Password?",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
             ],
-            )
-        )
-      )
+          ),
+        ),
+      ),
     );
   }
 }
