3c1d3a009684eebed8c3d166dca3fa50bfffbacf
Pak_Man926 <davidkamau926@gmail.com>
2025-03-26 15:34:48 +0300

    Eighth 7.1

--- lg_tribe/lib/Forgot Password/forgot_password.dart
+++ lg_tribe/lib/Forgot Password/forgot_password.dart
@@ -1,6 +1,31 @@
 import "package:flutter/material.dart";
 
+
 class Password extends StatelessWidget
 {
-  
+  @override
+  Widget build(BuildContext context)
+  {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              SizedBox(height:50),
+               Container(
+                height: 30,
+                alignment: Alignment.center,
+                padding: EdgeInsets.only(left: 50, right: 50, top: 50),
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(30)
+                ),
+               )
+            ],
+            )
+        )
+      )
+    );
+  }
 }
