b030d8775e06250254f4e2c33229f6c4b5086649
Pak_Man926 <davidkamau926@gmail.com>
2025-03-27 14:13:35 +0300

    Eighth 8.1

--- lg_tribe/lib/Forgot Password/forgot_password.dart
+++ lg_tribe/lib/Forgot Password/forgot_password.dart
@@ -1,6 +1,6 @@
 import "package:flutter/material.dart";
-import "package:phonenumber_input/phonenumber_input.dart";
-import 'package:intl_phone_number_input/intl_phone_number_input.dart';
+import 'package:intl_phone_field/intl_phone_field.dart';
+import 'package:lg_tribe/Login/login.Dart';
 
 class Password extends StatelessWidget {
   const Password({super.key});
@@ -11,63 +11,82 @@ class Password extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: SafeArea(
-          child: Column(
-            //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              SizedBox(height: 120),
-              Container(
-                //height: 50,
-                //width: 50,
-                alignment: Alignment.center,
-                padding: EdgeInsets.only(left: 50, right: 50),
-                decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 175, 76, 92),
-                  //borderRadius: BorderRadius.circular(50),
-                  shape: BoxShape.circle,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 120),
+                Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(20),
+                  decoration: const BoxDecoration(
+                    color: Color.fromARGB(255, 175, 76, 92),
+                    shape: BoxShape.circle,
+                  ),
+                  child: const Icon(Icons.key, size: 50, color: Colors.white),
                 ),
-                child: Icon(Icons.key, size: 50, color: Colors.white),
-              ),
-              SizedBox(height: 40),
-              Text(
-                "Forgot Password?",
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 10),
-              Text(
-                "No worries, we'll send in your reset instructions",
-                style: TextStyle(fontSize: 12, fontWeight: FontWeight.w100),
-              ),
-              SizedBox(height: 20),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 20),
+                const SizedBox(height: 40),
+                const Text(
+                  "Forgot Password?",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  "No worries, we'll send you reset instructions",
+                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.w100),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 20),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Registered Phone Number",
+                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+                  ),
+                ),
+                const SizedBox(height: 8),
+                IntlPhoneField(
+                  decoration: const InputDecoration(
+                    hintText: "Enter your phone number",
+                    border: OutlineInputBorder(),
+                  ),
+                  initialCountryCode: 'KE',
+                  // onChanged: (phone) {
+                  //   print(phone.completeNumber);
+                  // },
+                ),
+                const SizedBox(height: 20),
+                SizedBox(
+                  width: double.infinity,
+                  child: MaterialButton(
+                    onPressed: () {},
+                    color: const Color.fromARGB(255, 206, 25, 12),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.vertical(),
+                    ),
                     child: Text(
-                      "Registered Phone Number",
-                      style: TextStyle(
-                        fontSize: 12,
-                        fontWeight: FontWeight.w600,
-                      ),
+                      "Verify number",
+                      style: TextStyle(color: Colors.white),
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 20),
-                    child: InternationalPhoneNumberInput(
-                      onInputChanged: (PhoneNumber number) {
-                        print(number.phoneNumber);
-                      },
-                      onInputValidated: (bool value) {
-                        print(value);
-                      },
-                      selectorConfig: SelectorConfig(
-                        selectorType: PhoneInputSelectorType.BOTTOM_SHEET,
-                      ),
-                    ),
+                ),
+                const SizedBox(height: 10),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Login()),
+                    );
+                  },
+                  icon:Icon(Icons.arrow_back, color: Colors.white),
+                  child: const Text(
+                    "Back to login",
+                    style: TextStyle(color: Colors.white),
                   ),
-                ],
-              ),
-            ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
